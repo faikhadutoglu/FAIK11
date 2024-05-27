da662e9c01bed099449cea4b1ba6ecd3a744b4b7
faikhadutoglu <167749575+faikhadutoglu@users.noreply.github.com>
2024-05-27 12:30:47 +0000

    wrgf

--- Aufgaben/stl_and_casting_exercise.cpp
+++ Aufgaben/stl_and_casting_exercise.cpp
@@ -78,3 +78,46 @@ auto main() -> int
     }
     return 0;
 }
+
+/*// Definition der Hauptfunktion mit Rückgabetyp 'int'
+auto main() -> int
+{
+    // Erstellung eines Vektors, der shared pointers auf Animal-Objekte speichert
+    std::vector<std::shared_ptr<Animal>> animals;
+
+    // Initialisierung einer Quelle für Zufallszahlen
+    std::random_device seed;   // Eine Zufallszahlenquelle für die Initialisierung des Zufallszahlengenerators
+
+    // Initialisierung eines Mersenne-Twister-Zufallszahlengenerators mit der zuvor erstellten Zufallsquelle
+    std::mt19937 gen(seed());  // Ein Mersenne-Twister-Generator, der mit 'seed()' initialisiert wird
+
+    // Erstellung einer Verteilung, die gleichmäßig 0 oder 1 generiert
+    std::uniform_int_distribution<> distribution(0, 1); // Diese Verteilung generiert gleichmäßig Zahlen zwischen 0 und 1
+
+    // Schleife, die 10 Mal durchlaufen wird
+    for (int i = 0; i < 10; ++i)
+    {
+        // Bestimmung, ob ein Cat- oder ein Bird-Objekt erstellt wird, basierend auf der generierten Zufallszahl
+        if (static_cast<bool>(distribution(gen))) // Konvertiert das Ergebnis der Verteilung (0 oder 1) in einen boolschen Wert
+        {
+            // Wenn die Verteilung 1 zurückgibt (true), wird ein neues Cat-Objekt erstellt und dem Vektor hinzugefügt
+            animals.push_back(std::make_shared<Cat>());
+        }
+        else
+        {
+            // Wenn die Verteilung 0 zurückgibt (false), wird ein neues Bird-Objekt erstellt und dem Vektor hinzugefügt
+            animals.push_back(std::make_shared<Bird>());
+        }
+    }
+
+    // Schleife, die durch den Vektor 'animals' iteriert
+    for (auto &itr : animals)
+    {
+        // Aufruf der make_noise() Methode für jedes Element im Vektor
+        itr->make_noise();
+    }
+
+    // Die Funktion gibt 0 zurück, was den erfolgreichen Abschluss des Programms signalisiert
+    return 0;
+}
+*/
